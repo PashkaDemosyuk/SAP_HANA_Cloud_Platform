c3a4c0135f29e4c5a7ee86e8263ce5d7cbe74cb4
Pasha Demosyuk <31686138+PashkaDemosyuk@users.noreply.github.com>
2019-01-11 21:07:14 +0300

    First Task...

---
+++ Practice Training/Task1/Task1.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 11; i++) {
+    var el = document.createElement("div");
+    el.classList.add("card");
+    el.innerHTML = '<div class="card-head"><img src="source/image.jpeg"><div class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit.<div class="small-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, odio. Repudiandae dolores fuga praesentium quos, eligendi excepturi nobis rem ratione odit blanditiis unde, doloremque commodi illum atque ipsa accusantium adipisci.</div></div></div><button class="card-button">Try</button>';
+    var cont = document.getElementById("main");
+    cont.appendChild(el);
+}
+
+function goto() {
+    window.open('https://leverx.ru/', '_self');
+}
