2ae3c2b926065433914c1ba9e4dc42e92f7ac512
Pasha Demosyuk <31686138+PashkaDemosyuk@users.noreply.github.com>
2019-01-24 14:15:27 +0300

    Small Code Refactoring...

--- HW7/js/Task.js
+++ HW7/js/Task.js
@@ -3,139 +3,97 @@ function GETRequest() {
 
     var xhr = new XMLHttpRequest();
     //xhr.withCredentials = true;
-    
-    
-    
+
     xhr.open("GET", "https://cors-anywhere.herokuapp.com/http://services.odata.org/V4/TripPinService/People");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("cache-control", "no-cache");
     xhr.setRequestHeader("Postman-Token", "5d270113-0c88-4d27-8b8d-aca62836d56c");
     xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === 4) {
-        console.log(this.responseText);
-      }
+        if (this.readyState === 4) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+            $("#data tbody").remove();
+            var el = document.getElementById("data");
+
+            Array.from(json.value).forEach(element => {
+                el.innerHTML += '<tr>' +
+                    '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataUpdate(this)">Update</button></td>' +
+                    '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataDelete(this)">Delete</button></td>' +
+                    '<td>' + element.UserName + '</td>' +
+                    '<td>' + element.FirstName + '</td>' +
+                    '<td>' + element.LastName + '</td>' +
+                    '<td>' + element.Gender + '</td></tr>';
+            });
+
+        }
     });
     xhr.send(data);
 }
 
-
-//     var data = null;
-//     var el = document.getElementById("data");
-//     var xhr = new XMLHttpRequest();
-//     xhr.withCredentials = true;
-//
-//     xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === 4 && xhr.status === 200) {
-//             $("#data tbody").remove();
-//             console.log(this.responseText);
-//             var json = JSON.parse(xhr.responseText);
-//             Array.from(json).forEach(element => {
-//                 el.innerHTML += '<tr>' +
-//                     '<td><button class="btn btn-dark" id="' + element.id + '" onclick="DataUpdate(this)">Update</button></td>' +
-//     }                '<td><button class="btn btn-dark" id="' + element.id + '" onclick="DataDelete(this)">Delete</button></td>' +
-//                     '<td>' + element.UserName + '</td>' +
-//                     '<td>' + element.FirstName + '</td>' +
-//                     '<td>' + element.LastName + '</td>' +
-//                     '<td>' + element.Gender + '</td>' +
-//                     '</tr>';
-//             });
-//         }
-//     });
-//
-//     xhr.open("GET", "http://services.odata.org/V4/TripPinService/People");
-//     xhr.setRequestHeader("cache-control", "no-cache");
-//     xhr.setRequestHeader("Postman-Token", "da570e73-0244-4107-a3b4-b2b06bae7210");
-//     xhr.send(data);
-
-
-
-
-
-
-
-
-    // var el = document.getElementById("data");
-    // var url = "http://localhost:3000/data";
-    // var xhr = new XMLHttpRequest();
-    //
-    // xhr.addEventListener("readystatechange", function () {
-    //     if (xhr.readyState === 4 && xhr.status === 200) {
-    //         $("#data tbody").remove();
-    //         var json = JSON.parse(xhr.responseText);
-    //         Array.from(json).forEach(element => {
-    //             el.innerHTML += '<tr>' +
-    //                 '<td><button class="btn btn-dark" id="' + element.id + '" onclick="DataUpdate(this)">Update</button></td>' +
-    //                 '<td><button class="btn btn-dark" id="' + element.id + '" onclick="DataDelete(this)">Delete</button></td>' +
-    //                 '<td>' + element.userId + '</td>' +
-    //                 '<td>' + element.id + '</td>' +
-    //                 '<td>' + element.title + '</td>' +
-    //                 '<td>' + element.body + '</td>' +
-    //                 '</tr>';
-    //         });
-    //     }
-    // });
-    //
-    // xhr.open("GET", url);
-    // xhr.setRequestHeader("Content-Type", "application/json");
-    // xhr.send(data);
-// }
-
-// function POSTRequest() {
-//     var url = "http://localhost:3000/data";
-//     var formElements = document.getElementById("Form").elements;
-//     var data = {};
-//     for (var i = 0; i < formElements.length; i++) {
-//         data[formElements[i].name] = formElements[i].value;
-//     }
-//     var xhr = new XMLHttpRequest();
-//     xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === 4 && xhr.status === 200) {
-//
-//         }
-//     });
-//
-//     xhr.open("POST", url);
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.send(JSON.stringify(data));
-//     GETRequest();
-// }
-//
-function DataUpdate(obj) {
-    sessionStorage.setItem('idUpdate', obj.id);
-    var xhr = new XMLHttpRequest();
-    var url = "http://localhost:3000/data";
-    var itemId = sessionStorage.getItem('idUpdate');
+function POSTRequest() {
     var formElements = document.getElementById("Form").elements;
     var data = {};
     for (var i = 0; i < formElements.length; i++) {
         data[formElements[i].name] = formElements[i].value;
     }
 
-    xhr.open("PUT", url + '/' + itemId, false);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-        }
-    };
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://cors-anywhere.herokuapp.com/http://services.odata.org/V4/TripPinService/People");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            alert(data.UserName + ' was added (затычка)');
+            var el = document.getElementById("data");
+            var json = JSON.parse(xhr.responseText);
+            Array.from(json.value).forEach(element => {
+                el.innerHTML += '<tr>' +
+                    '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataUpdate(this)">Update</button></td>' +
+                    '<td><button class="btn btn-dark" id="' + element.UserName + '" onclick="DataDelete(this)">Delete</button></td>' +
+                    '<td>' + element.UserName + '</td>' +
+                    '<td>' + element.FirstName + '</td>' +
+                    '<td>' + element.LastName + '</td>' +
+                    '<td>' + element.Gender + '</td></tr>';
+            });
 
-    xhr.send(JSON.stringify(data));
-    GETRequest();
+        }
+    });
+
+    xhr.send(data);
+
+}
+
+function DataUpdate(obj) {
+    var formElements = document.getElementById("Form").elements;
+    var data = {};
+    for (var i = 0; i < formElements.length; i++) {
+        data[formElements[i].name] = formElements[i].value;
+    }
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://services.odata.org/V4/(S(4bctqhjqdedzwim1pxenlomc))/TripPinServiceRW/People"
+    xhr.open("PUT", url + '\'(' + obj.id + '\')');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            alert(obj.id + ' updated (затычка)');
+        }
+    });
+
+    xhr.send(data);
 }
 
 function DataDelete(obj) {
-    sessionStorage.setItem('idDelete', obj.id);
     var xhr = new XMLHttpRequest();
-    var url = "http://localhost:3000/data";
-    var itemId = sessionStorage.getItem('idDelete');
-
-    xhr.open("DELETE", url + '/' + itemId, false);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+    var url = "https://services.odata.org/V4/(S(4bctqhjqdedzwim1pxenlomc))/TripPinServiceRW/People"
+    xhr.open("DELETE", url + '\'(' + obj.id + '\')');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            alert(obj.id + ' deleted (затычка)');
         }
-    };
-    xhr.send();
-    GETRequest()
+    });
+
+    xhr.send(data);
 }
 
 
